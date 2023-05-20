5d1987a936d83ccf185543f2ebb23f01ac31787f
vexus95 <vexus95@outlook.fr>
2023-05-20 21:57:21 +0200

    Ajout liste

--- Traitement.js
+++ Traitement.js
@@ -1,8 +1,9 @@
-import React,{useState} from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, View ,TextInput,TouchableOpacity,Keyboard} from 'react-native';
+import React,{useEffect, useState} from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View , ScrollView, TextInput,TouchableOpacity,Keyboard} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {DayPicker} from 'react-native-picker-weekday'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import LoginScreen from './auth'
@@ -10,13 +11,56 @@ import CalendarPage from'./Calendar'
 import { ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
 import Task from './medicament'
 import addForm from './addMeds';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const Calendar = ({weekdays}) => {
+  return (
+    <DayPicker
+      weekdays={weekdays}
+      activeColor='#2980B9'
+      textColor='white'
+      inactiveColor='#BFC9CA'
+    />
+  )
+}
+
+const TraitementItem = ({traitementItem, index, deleteMed}) => {
+  return (
+    <TouchableOpacity key={index} onPress={() => deleteMed(index)}>
+      <Task text={traitementItem.med_name} />
+      <Task text={traitementItem.heure} />
+      <Calendar weekdays={traitementItem.date} />
+    </TouchableOpacity>
+  )
+}
 
 //navbar
 function Traitement({navigation}) {
   const[medicament,setMedicament] = useState();
   const[medItem,setMedItem] = useState([]);
+  const[fetch, setFetch] = useState(false);
+  const[traitementList, setTraitementList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async function() {
+      console.log("test");
+      const jsonValueString = await AsyncStorage.getItem('traitementList');
+      setTraitementList(jsonValueString != null ? JSON.parse(jsonValueString) : [])
+    };
+
+    fetchData();
+  }, [fetch]);
+
+  const deleteMed = async (index) => {
+    let itemsCopy = [...traitementList];
+    itemsCopy.splice(index, 1);
+    await AsyncStorage.setItem('traitementList', JSON.stringify(itemsCopy));
+    console.log("je supprime une donnée")
+    setTraitementList(itemsCopy);
+  }
 
   const handleAddMeds=()=>{
     Keyboard.dismiss();
@@ -24,35 +68,36 @@ function Traitement({navigation}) {
     setMedicament(null);
   }
 
-  const deleteMed = (index) =>{
-    let itemsCopy=[...medItem]; 
-    itemsCopy.splice(index,1);
-    setMedItem(itemsCopy);
-  }
+  // const deleteMed = (index) =>{
+  //   let itemsCopy=[...medItem]; 
+  //   itemsCopy.splice(index,1);
+  //   setMedItem(itemsCopy);
+  // }
+
     return (
       <View style={styles.container}>
         <View style={styles.taskWrapper}>
-          <View style={styles.items}>
+          <ScrollView style={styles.items}>
             {
-              medItem.map((item,index)=>{
+              traitementList.map((item,index)=>{
                 return (
-                  <TouchableOpacity key={index} onPress={()=>deleteMed(index)}>
-                    <Task text={item}/>
-                  </TouchableOpacity>
-
+                  // <TouchableOpacity key={index} onPress={()=>deleteMed(index)}>
+                  //   <Task text={item}/>
+                  // </TouchableOpacity>
+                  <TraitementItem traitementItem={item} index={index} deleteMed={deleteMed}/>
                 )
               })
             }
-          </View>
+          </ScrollView>
         </View>
         {/* Ajouter un traitement */}
         <KeyboardAvoidingView
           behavior = {Platform.OS === "ios" ? "padding" : "height"}
           style={styles.writeTaskWrapper}>
 
-          <TextInput style={styles.input} placeholder={'Ajoutez un traitement'} value={medicament} onChangeText={text=>setMedicament(text)}/>
+          {/* <TextInput style={styles.input} placeholder={'Ajoutez un traitement'} value={medicament} onChangeText={text=>setMedicament(text)}/> */}
 
-          <TouchableOpacity onPress={()=>navigation.navigate('addMed')}>
+          <TouchableOpacity onPress={()=> {setFetch(prevState => !prevState); navigation.navigate('addMed')}}>
               <View style={styles.addWrapper}>
                 <Text style={styles.addText}>+</Text>
               </View>
